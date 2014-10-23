0db7a9ec588391b198404d1ad1e68b6b9cd8c9d7
Denis <shklyannik.denis@gmail.com>
2014-10-23 18:57:45 +0300

    Second Commit

---
+++ TestQa/nester.py
@@ -0,0 +1,13 @@
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol(movies)
+
