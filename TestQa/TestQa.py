3339f1c4a8f9a1288c80cb9d9a7e0ee89fe0d294
OShine <shklyannik.denis@gmail.com>
2015-05-28 21:49:08 +0300

    Test

--- TestQa/TestQa.py
+++
@@ -1,8 +0,0 @@
-minutes = int(raw_input())
-a = minutes % 60
-if minutes < 60:
-    print(str(0) + " hours")
-    print(str(a) + " minutes")
-else:
-    print(str(minutes/60) + " hours")
-    print(str(a) + " minutes")
