a4149623465067f3415de4aabb33c99eddf24de1
d.shklyannik <shklyannik@gmail.com>
2015-12-09 16:47:05 +0300

    Final Commit with fixes.

---
+++ TestQa/CheckTheValueOfNumbers.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        x = int(raw_input("Enter the number for check: "))
+    except ValueError:
+        print 'The entered value should be an integer. Please, try again.'
+    else:
+        if x < 0:
+            x = 0
+            print 'The negative one, value has changed to zero.'
+        elif x == 0:
+            print 'Zero.'
+        elif x == 1:
+            print 'The One.'
+        else:
+            print 'The entered value is more than One.'
