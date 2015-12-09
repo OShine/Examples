0e66aa69c196b61122f71f78958d90821f44be2e
d.shklyannik <shklyannik@gmail.com>
2015-12-09 16:54:39 +0300

    Final Commit with fixes.

--- TestQa/HoursAndMinutes.py
+++ TestQa/HoursAndMinutes.py
@@ -1,8 +1,14 @@
-minutes = int(raw_input())
-a = minutes % 60
-if minutes < 60:
-    print(str(0) + " hours")
-    print(str(a) + " minutes")
-else:
-    print(str(minutes/60) + " hour")
-    print(str(a) + " minutes")
+while True:
+    try:
+        print 'Please, enter the value of time as integer'
+        minutes = int(raw_input())
+    except ValueError:
+         print 'The entered value should be an integer. Please, try again.'
+    else:
+        a = minutes % 60
+        if minutes < 60:
+            print(str(0) + " hours")
+            print(str(a) + " minutes")
+        else:
+            print(str(minutes/60) + " hour")
+            print(str(a) + " minutes")
