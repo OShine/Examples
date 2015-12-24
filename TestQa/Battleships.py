1019bddd271e4e36e4fe1873afd0f6f27a7c62a2
d.shklyannik <shklyannik@gmail.com>
2015-12-24 11:48:09 +0300

    Battleships

--- TestQa/Battleships.py
+++ TestQa/Battleships.py
@@ -1,5 +1,11 @@
 from random import randint
 
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
 board = []
 
 for x in range(5):
@@ -11,12 +17,6 @@ def print_board(board):
 
 print "Let's play Battleship!"
 print_board(board)
-
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
