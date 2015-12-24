5af7f435470d279c05c5c3d140ec228d2abd379e
d.shklyannik <shklyannik@gmail.com>
2015-12-24 11:39:28 +0300

    Battleships

--- TestQa/Temp.py
+++ TestQa/Temp.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 import os, sys
-for n in xrange(2, 10):
-    for x in xrange(2, n):
-        if n % x == 0:
-            print n,'=', x, '*', n/x
-            break
-        else:
-            print n, '- простое число'
+
+def fib(n):
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
+print fib(2000)
