9422912f257e619ecc01064effa13dce5f9c49eb
Denis <shklyannik.denis@gmail.com>
2016-03-23 21:56:17 +0300

    Python 3.5

--- TestQa/IntegerOrNot.py
+++ TestQa/IntegerOrNot.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
-import os, sys
+import os
+import sys
 
 while True:
     try:
-        x = int(raw_input("Enter the number for check: "))
+        x = int(input("Enter the number for check: "))
     except ValueError:
-        print 'The entered value should be an integer. Please, try again.'
+        print('The entered value should be an integer. Please, try again.')
     else:
         if x < 0:
             x = 0
-            print 'The negative one, value has changed to zero.'
+            print('The negative one, value has changed to zero.')
         elif x == 0:
-            print 'Zero.'
+            print('Zero.')
         elif x == 1:
-            print 'The One.'
+            print('The One.')
         else:
-            print 'The entered value is more than One.'
+            print('The entered value is more than One.')
