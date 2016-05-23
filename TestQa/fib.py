9b1c0ade5034c955ccd26f32877287e4bb9488e2
d.shklyannik <shklyannik@gmail.com>
2016-05-23 16:20:17 +0300

    3.5

--- TestQa/fib.py
+++ TestQa/fib.py
@@ -2,11 +2,14 @@
 # -*- coding: latin-1 -*-
 import os, sys
 
+
 def fib(n):
     result = []
     a, b = 0, 1
     while b < n:
         result.append(b)
-        a, b = b, a+b
+        a, b = b, a + b
     return result
-print (fib(2000))
+
+
+print(fib(2000))
