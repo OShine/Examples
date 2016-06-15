e5d27ee9ebf4d52c09e3f2eadc4397439757e54c
d.shklyannik <shklyannik@gmail.com>
2016-06-15 16:57:59 +0300

    3.5 Yellow-Duck.net Методы и данные

---
+++ TestQa/YouTube/house.py
@@ -0,0 +1,29 @@
+from TestQa.YouTube.person import *
+
+
+class House:
+    __address = "n/a"
+    __list_of_residents = ["n/a"]
+
+    def set_address(self, a):
+        self.__address = a
+
+    def get_address(self):
+        return self.__address
+
+    def settle_person(self, p):
+        if isinstance(p, Person):
+            self.__list_of_residents.append(p)
+            p.set_address(self.__address)
+
+    def evict_person(self, p):
+        self.__list_of_residents.remove(p)
+        p.set_address("n/a")
+
+    def description_of_house(self):
+        print("\n")
+        print("Address of this house is: ", self.__address)
+        print("List of residents:")
+        for p in self.__list_of_residents:
+            if isinstance(p, Person):
+                print("# -", p.get_name())
--- TestQa/YouTube/main.py
+++ TestQa/YouTube/main.py
@@ -3,16 +3,32 @@
 # -*- coding: latin-1 -*-
 
 
-from person import Person
+from TestQa.YouTube.person import Person
+from TestQa.YouTube.house import House
 
 # обявление объекта класса
 person1 = Person()
 person2 = Person()
 
+house = House()
+house.set_address("1Street")
+
 person1.set_name("John")
 person1.set_age("23")
-person1.description_of_person()
+person1.set_address("New York")
+
 
 person2.set_name("Mike")
 person2.set_age("35")
+person2.set_address("San Francisco")
+
+house.settle_person(person1)
+house.settle_person(person2)
+
+person1.description_of_person()
 person2.description_of_person()
+
+house.description_of_house()
+
+person1.testStatic()
+person1.testClass()
